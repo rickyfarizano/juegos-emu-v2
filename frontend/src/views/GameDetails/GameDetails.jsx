43e41cb1b8459d8b6f543d686c3cc2f808f89159
rickyfarizano <rickyfarizano@gmail.com>
2024-11-26 18:50:15 -0300

    feat:game details styles

--- frontend/src/views/GameDetails/GameDetails.jsx
+++ frontend/src/views/GameDetails/GameDetails.jsx
@@ -10,30 +10,30 @@ const GameDetails = () => {
 
       <div className="container">
 
-        <div className="game-detals__content">
-
           <div className="game-details__title-img">
             {/* TITULO DINAMICO */}
-            <h1 className="game-details__title">Titulo del juego</h1>
+            <h1 className="game-details__title text-3xl font-semibold mb-4 text-center text-white ">Titulo del juego</h1>
 
-            <figure className="game-details__img-banner">
-              <img src="#" alt="Imagen dle juego" />
+            <figure className="game-details__img-banner w-100 p-4">
+              <img src="/public/images/dragon-ball-banner.jpg" alt="Imagen dle juego" />
             </figure>
           </div>
+        <div className="game-detals__content border m-4">
 
-          <article className="game-details__description">
-            <h2 className="title">Descripicon del juego</h2>
+
+          <article className="game-details__description p-4">
+            <h2 className="title text-2xl font-semibold mb-4 text-white">Descripicon del juego</h2>
 
             {/* INFO DINAMICA */}
-            <p className="details-description">
+            <p className="details-description text-white">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, facilis! Deleniti neque sed eligendi dolor 
               ratione dignissimos officia voluptatum repellat culpa odio explicabo necessitatibus, quo, dolorem nostrum?
                Repellendus, nostrum atque!
             </p>
           </article>
 
-          <article className="game-details__screenshots">
-            <h2 className="title">Screenshots</h2>
+          <article className="game-details__screenshots p-4">
+            <h2 className="title text-2xl font-semibold mb-4 text-white">Screenshots</h2>
             <div className="screenshots-container">
               {/* carga dinamica de imagenes */}
               <figure className="screehshot">
@@ -42,22 +42,24 @@ const GameDetails = () => {
             </div>
           </article>
 
-          <article className="game-requirements">
-            <h2 className="title">Requerimientos del sistema</h2>
+          <article className="game-requirements p-4">
+            <h2 className="title text-2xl font-semibold mb-4 text-white">Requerimientos del sistema</h2>
             <div className="list-container">
               {/* carga dinamica de requerimientos */}
               <ul className="req-list">
-                <li>item 1</li>
-                <li>item 2</li>
-                <li>item 3</li>
-                <li>item 4</li>
-                <li>item 5</li>
+                <li className="text-white">Procesador: Intel Core i3-9100F</li>
+                <li className="text-white">Tarjeta gráfica: NVIDIA GTX 750 Ti</li>
+                <li className="text-white">RAM: 4 GB DDR3</li>
+                <li className="text-white">Almacenamiento: HDD de 500 GB</li>
+                <li className="text-white">Sistema operativo: Windows 10 de 64 bits</li>
               </ul>
             </div>
           </article>
 
-          {/* Agregar link de descarga dinamicamente */}
-          <a href="#" className="btn-download">Descargar juego</a>
+          <div className="btn w-100 flex justify-center items-center p-4">
+            {/* Agregar link de descarga dinamicamente */}
+            <a href="#" className="btn-download text-center p-3 rounded bg-blue-900 hover:bg-blue-800 text-white transition-all">Descargar juego</a>
+          </div>
         </div>
 
       </div>
