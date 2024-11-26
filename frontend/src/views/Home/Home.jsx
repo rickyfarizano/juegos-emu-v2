1ddb5e119f6568ac6eb0a881366ea5c2fe5bc2cd
rickyfarizano <rickyfarizano@gmail.com>
2024-11-26 17:34:44 -0300

    feat:general structure of home

--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -4,22 +4,46 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home-container">
-      {/* Banner lateral izquierdo */}
-      {/* <div className="left-banner">
-        <img src="/images/god-of-war-izquierda.jpg" alt="Left Banner" />
-      </div> */}
+    <section className="home-container">
+      <div className="container">
 
-      {/* Contenido principal (Slider en el centro) */}
-      <div className="main-content">
-        <Slider />
+        <section className="carousel-banner">
+          <Slider />
+        </section>
+
+        {/* aca van a ir los distintos grupos de juegos separados por genero */}
+        <section className="game-groups m-4 flex gap-4 flex-col">
+
+          {/* reemplazar desde ACA ============ */}
+          <div className="game bg-neutral-700 p-4">
+            <h2 className="group-title text-white text-4xl mb-2">Accion</h2>
+
+            <div className="container-groups">
+              <p className='text-white'>aca iria el carousel con los juegos correspondientes a su genero</p>
+            </div>
+          </div>
+          {/* hasta ACA ============ */}
+
+          <div className="game bg-neutral-700 p-4">
+            <h2 className="group-title text-white text-4xl mb-2">Aventura</h2>
+
+            <div className="container-groups">
+              <p className='text-white'>aca iria el carousel con los juegos correspondientes a su genero</p>
+            </div>
+          </div>
+
+          <div className="game bg-neutral-700 p-4">
+            <h2 className="group-title text-white text-4xl mb-2">RPG</h2>
+
+            <div className="container-groups">
+              <p className='text-white'>aca iria el carousel con los juegos correspondientes a su genero</p>
+            </div>
+          </div>
+
+        </section>
+
       </div>
-
-      {/* Banner lateral derecho */}
-      {/* <div className="right-banner">
-        <img src="/images/the-witcher-derecha.jpg" alt="Right Banner" />
-      </div> */}
-    </div>
+    </section>
   );
 };
 
