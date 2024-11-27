cbd265041684c880280ec3323b4dc9a62f96b2ef
rickyfarizano <rickyfarizano@gmail.com>
2024-11-26 22:51:57 -0300

    feat:adding new game schema

--- backend/models/Game.js
+++ backend/models/Game.js
@@ -12,11 +12,19 @@ const gameSchema = new mongoose.Schema({
       message: 'El año de lanzamiento debe estar entre 1980 y el año actual',
     },
   },
-  
   rating: { type: Number },
   description: { type: String },
   weight: { type: Number, required: true },
   image: { type: String },  // Guardará la ruta de la imagen
+  developer: { type: String }, // Nuevo campo para el desarrollador
+  youtubeUrl: { type: String }, // Nuevo campo para el URL de YouTube
+  gallery: { type: [String] }, // Nuevo campo para la galería de imágenes (array de strings)
+  requirements: { 
+    gpu: { type: String }, // Requisitos del GPU
+    ram: { type: String }, // Requisitos de RAM
+    cpu: { type: String }, // Requisitos del CPU
+  },
+  downloadLink: { type: String }, // Nuevo campo para el enlace de descarga
 });
 
 const Game = mongoose.model('Game', gameSchema);
